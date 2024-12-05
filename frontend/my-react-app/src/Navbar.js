42c773eab6d62cbbc429e5fdd29c5cdc8cd8a8c8
Anna Nguyen <annanguyn99@gmail.com>
2024-12-05 18:09:17 +1100

    Completed navigation bar

---
+++ frontend/my-react-app/src/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <a href="/" className="logo">MindFlow</a>
+            <div className="navbar-links">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">Journal</a></li>
+                    <li><a href="/">Login/Signup</a></li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
